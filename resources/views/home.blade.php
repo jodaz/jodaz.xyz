15ccc282b2a3efa25544b4c6006bffe678044951
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-05-31 16:28:25 -0400

    Add media print to google fonts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         {!! SEO::generate() !!}
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
         <link href="{{ asset('css/website/main.css') }}" rel="stylesheet">
     </head>
     <body>
