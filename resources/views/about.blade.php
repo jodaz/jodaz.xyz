86f6cd050f715f219ac5ba4ebdb1fdf1e1172c31
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-06-04 14:11:02 -0400

    Update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,24 +1,18 @@
 @extends('layout.template')
 
 @section('content')
-<section class="flex about" id="about">
-    <div class="about-title">
-        <h2>WhoAmI</h2>
-    </div><div class="about-photo">
-        <img src="{{ asset('/assets/img/profile.jpg') }}">
-    </div>
-    <div class="about-bio">
-        <p>
-            I am a web developer based in Carupano, Venezuela.
-        </p>
-        <p>I love to solve problems as well as making things pretty and easy to use. I can't stop learning new things; the more, the better.
-        In my free time, I enjoy to design and code interfaces, algorithm exercises and web development projects.
-        </p>
-        <p>For the front-end I usually work with Javascript, either standalone or including a frammework like ReactJS.
-        I also make the web pretty by using CSS and, whenever needed, use a design frammework like Bootstrap.
-        </p>
-        <p>For the back-end I work with Javascript, Node.JS, Express and MongoDB.
-        </p>
-    </div>
+<section class="flex container container--light">
+    <h2 class="container-title">About</h2>
+    <p class="container-subtitle">
+        I am a web developer based in Carupano, Venezuela.
+    </p>
+    <p class="container-subtitle">I love to solve problems as well as making things pretty and easy to use. I can't stop learning new things; the more, the better.
+    In my free time, I enjoy to design and code interfaces, algorithm exercises and web development projects.
+    </p>
+    <p class="container-subtitle">For the front-end I usually work with Javascript, either standalone or including a frammework like ReactJS.
+    I also make the web pretty by using CSS and, whenever needed, use a design frammework like Bootstrap.
+    </p>
+    <p class="container-subtitle">For the back-end I work with Javascript, Node.JS, Express and MongoDB.
+    </p>
 </section>
 @endsection
