5cbb72084c9f822177f0e73442ab66b8d9e25e94
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-06-05 21:13:57 -0400

    Change font to Roboto slab

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <section class="flex container container--light">
     <div class="row">
         <p>I am a software developer passionate for building pretty websites and web applications at vanguard</p>
-        <p>I like to design my products from the grown up, from the data model to the user experience and interface.</p>
+        <p>I like to design my products from the grown up, from the data model to the user experience and interface, taking care of each detail.</p>
         <p>I specialize in the latest technology trends, using backend frammeworks as Laravel and Nodejs and frontend libraries as React.js</p>
         <p>Be sure to have a closer look at my <a href="{{ route('projects') }}" title="Projects" class="l ac">work</a> or get to know more about my <a href="{{ route('about') }}" class="l ac" title="About me">interests</a>.</p>
     </div>
--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -8,7 +8,7 @@
         <!-- Fonts -->
         <link href="{{ asset('css/website/main.css') }}" rel="stylesheet">
         <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
     </head>
     <body>
         <header class="flex header" id="header">
