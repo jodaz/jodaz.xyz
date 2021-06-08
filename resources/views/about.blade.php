d9c8bc9137e0d85470942daa628eca111faf4904
Jesus Ordosgoitty <jesuodz@gmail.com>
2021-06-08 09:58:34 -0400

    Add more translations

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,28 +6,30 @@
         <p>
             {{ __('Hello! My name is Jesus Ordosgoitty and I am a software developer based in Carupano, Venezuela.') }}
         </p>
-        <p>I love to fix problems, as well as making things pretty and easy to use through development and coding. Also I can't stop learning new things; the more, the better.
+        <p>
+        {{ __("I love to fix problems, as well as making things pretty and easy to use through development and coding. Also I can't stop learning new things; the more, the better.") }}
         </p>
         <p>
-            Want a downloable curriculum? Check the
+            {{ __("Want to see a downloadable curriculum? Check the versions") }}
             <a
                  href="{{ asset('assets/files/cv-en.pdf') }}"
                  title="English CV"
                  class="l ac"
                  target="_blank"
-            >English</a> or
+            >{{ __("English") }}</a>
+            {{ __("or") }}
             <a
                 href="{{ asset('assets/files/cv-es.pdf') }}"
                 title="English CV"
                 class="l ac"
                 target="_blank"
-            >Spanish</a> version.
+            >{{ __("Spanish") }}</a>.
         </p>
     </div>
 </section>
 <section class="flex container container--primary">
     <div class="row">
-        <h2 class="container--title"><span class="dpc s">#</span> Experience</h2>
+        <h2 class="container--title"><span class="dpc s">#</span> {{ __("Experience") }}</h2>
     </div>
     <div class="row">
         <div class="row">
@@ -38,49 +40,57 @@
         </div>
     </div>
     <div class="row">
-        <h2 class="container--title"><span class="dpc s">#</span> Uses</h2>
+        <h2 class="container--title"><span class="dpc s">#</span> {{ __("Uses") }}</h2>
         <div class="row">
-            <h3>Systems, Languages and Frammeworks</h3>
+            <h3>{{ __("Systems, Languages and Frammeworks") }}</h3>
             <ul>
                 <li>
                     <a href="https://ubuntu.com/" class="l l--light">Ubuntu</a>
-                    and flavors are my preferred OS systems.
+                    {{ __("and flavors are my preferred OS systems.") }}
                 </li>
-                <li>Generally I deploy my apps with
+                <li>{{ __("Generally I deploy my apps with") }}
                     <a href="https://pm2.keymetrics.io/" class="l l--light">PM2</a>
-                    and
+                    {{ __("and") }}
                     <a href="https://httpd.apache.org/" class="l l--light">Apache2</a>
-                    web server.
+                    {{ __("web server.") }}
                 </li>
-                <li><a href="https://www.typescriptlang.org/" class="l l--light">Typescript</a> and <a href="https://www.php.net/" class="l l--light">PHP</a> have become my default tools for working.</li>
-                <li>I implement <a href="https://www.python.org/" class="l l--light">Python</a>,
-                <a href="https://en.wikipedia.org/wiki/Bash_(Unix_shell)" class="l l--light">Bash</a> and
-                <a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript" class="l l--light">Javascript</a> for task automation, utilities and migration tools.</li>
-                <li>As backend frammeworks, <a href="https://laravel.com/" class="l l--light">Laravel</a> and
+                <li><a href="https://www.typescriptlang.org/" class="l l--light">Typescript</a>
+                    {{ __("and") }}
+                    <a href="https://www.php.net/" class="l l--light">PHP</a>
+                    {{ __("have become my default tools for working.") }}
+                </li>
+                <li>{{ __("I implement") }} <a href="https://www.python.org/" class="l l--light">Python</a>,
+                <a href="https://en.wikipedia.org/wiki/Bash_(Unix_shell)" class="l l--light">Bash</a> {{ __("and") }}
+                <a href="https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript" class="l l--light">Javascript</a>
+                    {{ __("for task automation, utilities and migration tools.") }}
+                </li>
+                <li>{{ __("As backend frammeworks") }}, <a href="https://laravel.com/" class="l l--light">Laravel</a> {{ __("and") }}
                     <a href="https://expressjs.com/" class="l l--light">Express</a> /
                     <a href="https://nodejs.org/" class="l l--light">Node.js</a> are really useful.</li>
                 <li>
                     <a href="https://www.postgresql.org/" class="l l--light">PostgreSQL</a>,
-                    <a href="https://www.mongodb.com" class="l l--light">MongoDB</a> and
+                    <a href="https://www.mongodb.com" class="l l--light">MongoDB</a> {{ __("and") }}
                     <a href="https://www.mysql.com/" class="l l--light">MySQL</a>
-                    are my prefered database management systems.
+                    {{ __("are my prefered database management systems.") }}
                 </li>
-                <li>I found
-                    <a href="http://knexjs.org/" class="l l--light">Ubuntu</a>
-                    is a very useful library for upgrading or changing a database.
+                <li>{{ __("I found") }}
+                    <a href="http://knexjs.org/" class="l l--light">Knex.js</a>
+                    {{ __("is a very useful library for upgrading or changing a database.") }}
                 </li>
-                <li>Finally,
+                <li>{{ __("Finally") }},
                     <a href="https://reactjs.org/" class="l l--light">React.js</a>
-                    and <a href="https://marmelab.com/react-admin/" class="l l--light">React Admin</a>
-                    are my goto libraries for building user interfaces.
+                    {{ __("and") }} <a href="https://marmelab.com/react-admin/" class="l l--light">React Admin</a>
+                    {{ __("are my goto libraries for building user interfaces.") }}
                 </li>
-                <li>Should I mention I like <a href="https://redux.js.org/" class="l l--light">Redux</a> / <a href="https://redux-saga.js.org/" class="l l--light">ReduxSaga</a>, <a href="https://final-form.org/react" class="l l--light">React Final Form</a>, <a href="https://reacttraining.com/react-router/" class="l l--light">React Router</a> and <a href="https://material-ui.com/" class="l l--light">MaterialUI</a> ?.</li>
+                <li>{{ __("Should I mention I like") }}
+                    <a href="https://redux.js.org/" class="l l--light">Redux</a> / <a href="https://redux-saga.js.org/" class="l l--light">ReduxSaga</a>,
+                    <a href="https://final-form.org/react" class="l l--light">React Final Form</a>, <a href="https://reacttraining.com/react-router/" class="l l--light">React Router</a> {{ __("and") }} <a href="https://material-ui.com/" class="l l--light">MaterialUI</a> ?.</li>
             </ul>
         </div>
         <div class="row">
             <h3>Editor</h3>
             <ul>
-                <li>I switch between Vim and VS Code for coding.</li>
+                <li>{{ __("I usually switch between Vim and VS Code for coding.") }}</li>
             </ul>
         </div>
         <div class="row">
--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -4,7 +4,7 @@
 <section class="flex container container--primary">
     <div class="row t--left">
         <h2 class="container--title"><span class="dpc s">#</span> Projects</h2>
-        <p>As an employee I'm currently working with local organizations of the region building the software infrastructure of the future, but I also do freelance and remote work for national companies and clients overseas.</p>
+        <p>{{ __("As an employee I'm currently working with local organizations of the region building the software infrastructure of the future, but I also do freelance and remote work for national companies and clients overseas.") }}</p>
     </div>
     <div class="row">
         <div class="row project">
@@ -33,13 +33,13 @@
 </section>
 <section class="flex container container--dark">
     <div class="row tc">
-        <h2 class="q">In every new venture, time must be our unit of account.</h2>
+        <h2 class="q">{{ __('In every new venture, time must be our unit of account.') }}</h2>
     </div>
 </section>
 <section class="flex container container--light">
     <div class="row t--left">
-        <h2 class="container--title"><span class="ac s">#</span> Packages and utilities</h2>
-        <p>Overtime, I had to build some stuff to customize frammeworks or just make my job a lot more easier and quickier. Here is some of my best repositories:</p>
+        <h2 class="container--title"><span class="ac s">#</span> {{ __('Packages and utilities') }}</h2>
+        <p>{{ __('Overtime, I had to build some stuff to customize frameworks or just make my job a lot more easier and quicker. Here is some of my best repositories') }}:</p>
     </div>
     <div class="row">
         <ul>
